5d5411cfc1b7f35fda7a93c60485e8e919b870e6
Monametsi <elboncaptain@gmail.com>
2024-12-20 09:32:32 +0200

    Arrays completed with find second smallest exercise

--- 02/02_07/02_07b/main.py
+++ 02/02_07/02_07b/main.py
@@ -1,1 +1,25 @@
 # Linear Search
+
+my_list = [8, 5, 0, 3, 9, 7]
+target = 7
+
+def linear_search(list, target):
+  for element in list:
+    if element == target:
+      return True
+  return False
+
+is_7_in = linear_search(my_list, target)
+is_5_in = linear_search(my_list, 5)
+is_1_in = linear_search(my_list, 1)
+
+print(is_7_in)
+print(is_5_in)
+print(is_1_in)
+
+
+#Array.sort() and sorted(Array) Functions
+arr = [3, 1, 6, 4, 2, 67, 5]
+print(sorted(arr))
+print(arr)
+#Output 
--- 02/02_10/02_10e/main.py
+++ 02/02_10/02_10e/main.py
@@ -1,8 +1,48 @@
+
 def find_second_smallest(my_list):
     if len(my_list) < 2:
         return None
     sorted_list = sorted(my_list)
     return sorted_list[1]
+
+
+
+def  find_second_smallone(lis):
+    lis = sorted(lis)
+    second_smallest = lis[1]
+    return second_smallest
+
+#Creating a random arraty for testing
+import random as rand
+def random_array():
+    '''Creates a random array'''
+    random_arr = rand.sample(range(1, 28), 7, counts=None)
+    return random_arr
+
+test_list = random_array()
+print(test_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def find_second_smallest_v2(my_list):
     if len(my_list) < 2:
